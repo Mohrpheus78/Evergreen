b85bb42d501e217f52f3e9879e33d4562c254c89
Dennis Mohrmann <dm@NB-DM-MAC-2.local>
2022-03-28 17:08:18 +0200

    Update Install WorkspaceApp LTSR Web.ps1

--- Software/Install WorkspaceApp LTSR Web.ps1
+++ Software/Install WorkspaceApp LTSR Web.ps1
@@ -75,7 +75,7 @@ try	{
 	}
 	New-Item -Path "HKCU:\SOFTWARE\Citrix\Splashscreen" -EA SilentlyContinue | Out-Null
 	New-ItemProperty -Path "HKCU:\Software\Citrix\Splashscreen" -Name SplashscrrenShown -Value 1 -PropertyType DWORD -EA SilentlyContinue | Out-Null
-	New-Item -Path "HKLM:\SOFTWARE\Wow6432Node\Policies\Citrix" -EA SilentlyContinue -EA SilentlyContinue | Out-Null
+	New-Item -Path "HKLM:\SOFTWARE\Wow6432Node\Policies\Citrix" -EA SilentlyContinue | Out-Null
 	New-Item -Path "HKLM:\SOFTWARE\Policies\Citrix" -EA SilentlyContinue | Out-Null
 	New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Policies\Citrix" -Name EnableX1FTU -Value 0 -PropertyType DWORD -EA SilentlyContinue | Out-Null
 	New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Citrix" -Name EnableFTU -Value 0 -PropertyType DWORD -EA SilentlyContinue | Out-Null
