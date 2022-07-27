f278d514a65075c1c02f504c2718949343492359
Dennis Mohrmann <dm@NB-DM-MAC-2.local>
2022-07-27 10:30:26 +0200

    Update Evergreen-Software Installer.ps1

--- Evergreen-Software Installer.ps1
+++ Evergreen-Software Installer.ps1
@@ -20,7 +20,7 @@ If you made your selection once, you can run the script with the -noGUI paramete
 Thanks to Trond Eric Haarvarstein, I used some code from his great Automation Framework! Thanks to Manuel Winkel for the forms ;-)
 There are no install scripts for VMWare Tools and openJDK yet!
 Run as admin!
-Version: 2.9.1
+Version: 2.9.2
 06/24: Changed internet connection check
 06/25: Changed internet connection check
 06/27: [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 at the top of the script
@@ -29,6 +29,7 @@ Version: 2.9.1
 06/30: Changed FileZilla version check
 07/04: Suppress error message while removing Teams from run key (if already removed)
 07/06: Citrix WorkspaceApp run keys prevented the app from starting at logon (Current Release)
+07/27: Changed Citrix WorkspaceApp version check and always install MS Edge WebView updates, changed Adobe DC update check
 #>
 
 Param (
@@ -95,7 +96,7 @@ ELSE {
 
 # Is there a newer Evergreen Script version?
 # ========================================================================================================================================
-[version]$EvergreenVersion = "2.9.1"
+[version]$EvergreenVersion = "2.9.2"
 $WebVersion = ""
 [bool]$NewerVersion = $false
 If ($Internet -eq "True") {
