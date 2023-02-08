66140afcdaf62add4be19f9884387aced5568844
Dennis Mohrmann <dm@NB-DM-MAC-2.local>
2023-02-08 13:31:00 +0100

    Update Install Citrix Server VDA for PVS CR.ps1

--- Software/Install Citrix Server VDA for PVS CR.ps1
+++ Software/Install Citrix Server VDA for PVS CR.ps1
@@ -71,7 +71,7 @@ IF (!(Test-Path "$InstDir\Software\Citrix\Current\CVAD")) {
 		Write-host -ForegroundColor Red "Installation path not valid, please check '$InstDir\Software\Citrix\Current\CVAD'!"
 		pause
 		BREAK }
-		Start-Process "$InstDir\Software\Citrix\Current\CVAD\x64\XenDesktop Setup\XenDesktopVdaSetup.exe" –ArgumentList "/NOREBOOT /exclude ""Personal vDisk"",""Machine Identity Service"",""Citrix Telemetry Service"",""Citrix Personalization for App-V -VDA"",""Citrix Files for Windows"",""Citrix Files for Outlook"",""User personalization layer"",""Workspace Environment Management"",""Citrix Rendezvous V2"",""Citrix VDA Upgrade Agent"" /COMPONENTS VDA /disableexperiencemetrics /enable_remote_assistance /enable_hdx_ports /enable_hdx_udp_ports /enable_real_time_transport /enable_ss_ports /masterpvsimage" –NoNewWindow -Wait
+		Start-Process "$InstDir\Software\Citrix\Current\CVAD\x64\XenDesktop Setup\XenDesktopVdaSetup.exe" –ArgumentList "/NOREBOOT /exclude ""Citrix Personalization for App-V - VDA"",""Machine Identity Service"",""Citrix Telemetry Service"",""Citrix VDA Upgrade Agent"" /COMPONENTS VDA /disableexperiencemetrics /enable_remote_assistance /enable_hdx_ports /enable_hdx_udp_ports /enable_real_time_transport /enable_ss_ports /masterpvsimage" –NoNewWindow -Wait
 		DS_WriteLog "-" "" $LogFile
 		Write-Host -ForegroundColor Green " ...ready!" 
 		Write-Output ""
