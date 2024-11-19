ae54bdc8941721963c4f898b38af67e33ca26ee3
Dennis Mohrmann <72930132+Mohrpheus78@users.noreply.github.com>
2024-11-19 14:48:13 +0100

    Update Evergreen-Software Installer.ps1

--- Evergreen-Software Installer.ps1
+++ Evergreen-Software Installer.ps1
@@ -19,7 +19,7 @@ If you made your selection once, you can run the script with the -noGUI paramete
 .NOTES
 Thanks to Trond Eric Haarvarstein, I used some code from his great Automation Framework! Thanks to Manuel Winkel for the forms ;-)
 Run as admin!
-Version: 2.17.12F
+Version: 2.17.12
 06/24: Changed internet connection check
 06/25: Changed internet connection check
 06/27: [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 at the top of the script
